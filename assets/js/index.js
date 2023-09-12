1d1620dadbf204def717a2535a14e70f6044b8aa
Vitor <vitorsouzasilva121@gmail.com>
2023-09-11 22:01:38 -0300

    add: add logic off requets infos from api and fill dynamically the cards, stylized the page

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,36 +5,33 @@ const characterList = document.getElementById("characterList")
 
 //Buscar Api
 async function fetchApi() {
-    const result = await fetch(`//rickandmortyapi.com/api/character/1`)
-        .then((res) => res.json()) // Transformando a resposta em JSON
-        .then((data) => data.results) // Buscando apenas os valores "results" do meu JSON
-        .then((characterResults) => {
-            console.log(characterResults);
-            return characterResults;
-        });
-    return result;
+    const result = await fetch(`https://rickandmortyapi.com/api/character/?page=1`)
+    /* .then((res) => res.json()) // Transformando a resposta em JSON
+    .then((data) => data.results) // Buscando apenas os valores "results" do meu JSON
+    .then((characters) => { */
+    const characters = await result.json()
+    return characters.results
 }
 
+async function addCharacters({ characters }) {
+    characters.forEach((character) => {
 
-async function addCharacters() {
-    const result = await fetchApi();
-    characterList.innerHTML = `
+        return characterList.innerHTML += `
         <li class="character">
-            <span class="id">${result.id}</span>
-            <span class="name">${result.name}</span>
-
-            <div class="detail">
-                <img src="${result.image}" alt="" id="image">
-                <ul class="infos">
-                    <li class="status">${result.status}</li>
-                    <li class="species">${result.species}</li>
-                </ul>
-            </div>
+            <img src="${character.image}" alt="" id="image">
+            <span class="name">${character.name}</span>
+                <div class="detail">
+                    <span class="species">${character.species}</span>
+                    <span class="${character.status}">${character.status}</span>
+                </div>
         </li>`
+    });
+}
 
-    // content.textContent = `${JSON.stringify(result, undefined, 2)}`;
+async function main() {
+    const characters = await fetchApi()
+    addCharacters({ characters })
 
 }
 
-addCharacters()
-
+main()
